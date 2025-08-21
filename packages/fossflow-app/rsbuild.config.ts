59d51ec5a0be809522bc7b53cd70a50fc8dffbe6
mu ziyu <61375428+AmazingRain@users.noreply.github.com>
2025-08-21 22:31:30 +0800

    feat-use-specify-template (#99)
    Thanks @AmazingRain

--- packages/fossflow-app/rsbuild.config.ts
+++ packages/fossflow-app/rsbuild.config.ts
@@ -3,6 +3,9 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
     plugins: [pluginReact()],
+    html: {
+        template: './public/index.html',
+    },
     output: {
         distPath: {
             root: 'build',
