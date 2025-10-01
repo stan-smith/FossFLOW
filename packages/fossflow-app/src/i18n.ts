fe10ec43c25d5cad62f4fb9b8ecbde5dd984655d
muziyu <2785745642@qq.com>
2025-10-01 21:35:20 +0800

    feat:save user language settings

--- packages/fossflow-app/src/i18n.ts
+++ packages/fossflow-app/src/i18n.ts
@@ -4,35 +4,34 @@ import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n
-    .use(Backend)
-    .use(LanguageDetector)
-    .use(initReactI18next)
-    .init({
-        fallbackLng: 'en-US',
-        debug: process.env.NODE_ENV === 'development',
-        interpolation: {
-            escapeValue: false,
-        },
-        ns: ['app'],
-        backend: {
-            loadPath: './i18n/{{ns}}/{{lng}}.json'
-        },
-        detection: {
-            // configure detection options
-            order: ['navigator', 'htmlTag', 'querystring', 'cookie', 'localStorage'],
-            caches: ['localStorage', 'cookie'],
-        }
-    });
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'en-US',
+    debug: process.env.NODE_ENV === 'development',
+    interpolation: {
+      escapeValue: false,
+    },
+    ns: ['app'],
+    backend: {
+      loadPath: './i18n/{{ns}}/{{lng}}.json'
+    },
+    detection: {
+      order: ['localStorage'],
+      caches: ['localStorage'],
+    }
+  });
 
 export const supportedLanguages = [
-    {
-        label: 'English',
-        value: 'en-US',
-    },
-    {
-        label: '中文',
-        value: 'zh-CN',
-    },
+  {
+    label: 'English',
+    value: 'en-US',
+  },
+  {
+    label: '中文',
+    value: 'zh-CN',
+  },
 ];
 
 export default i18n;
